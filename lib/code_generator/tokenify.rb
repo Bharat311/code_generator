6031295c012b0600cf5812e3173edb8330ed2d75
Bharat Gupta <bindassbharat311@gmail.com>
2013-06-09 02:20:38 +0530

    Added option :unique for tokenize method

--- lib/code_generator/tokenify.rb
+++ lib/code_generator/tokenify.rb
@@ -4,6 +4,9 @@ module CodeGenerator
   
     module ClassMethods
       def tokenify!(field, opts = {})
+        opts[:uniqueness] = { model: self.to_s.underscore, 
+                              field: field } if opts.delete(:unique)
+        
         after_initialize do |record|
           if record.send(field).blank?
             token = CodeGenerator::Generator.generate(opts)
@@ -12,5 +15,6 @@ module CodeGenerator
         end
       end
     end
+    
   end
 end
